425e92d4161c8be632e0fc34d43a94958a1597e0
Khangai.D <khangai0526@gmail.com>
2023-09-13 17:57:48 -0500

    Add startup-code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.mppproject {
     requires org.kordamp.bootstrapfx.core;
 //    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.mppproject to javafx.fxml;
     exports com.example.mppproject;
